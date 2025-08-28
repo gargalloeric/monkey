76230f785c0e303941a3fcd494896d27680588b5
gargalloeric <erickmontes555@gmail.com>
2025-08-28 20:30:45 +0200

    feat(ast): add ast nodes

---
+++ internal/ast/ast.go
@@ -0,0 +1,52 @@
+package ast
+
+import "github.com/gargalloeric/monkey/internal/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statement []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statement) > 0 {
+		return p.Statement[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type LetStatement struct {
+	Token token.Token // the token.LET token
+	Name  *Identifier
+	Value Expression
+}
+
+func (l *LetStatement) statementNode() {}
+
+func (l *LetStatement) TokenLiteral() string {
+	return l.Token.Literal
+}
+
+type Identifier struct {
+	Token token.Token // the token token.IDENT token
+	Value string
+}
+
+func (i *Identifier) expressionNode() {}
+
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
+}
