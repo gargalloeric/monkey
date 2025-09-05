bca6fb8d7b362dc91998b465c7c547dc4a461793
gargalloeric <erickmontes555@gmail.com>
2025-09-05 19:41:54 +0200

    feat(parser): add IntegerLiteral parsing function for prefix INT token type

--- internal/parser/parser_test.go
+++ internal/parser/parser_test.go
@@ -143,3 +143,34 @@ func TestIdentifierExpression(t *testing.T) {
 		t.Fatalf("ident.TokenLiteral not %s. got=%s", "foobar", ident.TokenLiteral())
 	}
 }
+
+func TestIntegerLiteralExpression(t *testing.T) {
+	input := "5;"
+
+	lexer := lexer.New(input)
+	parser := New(lexer)
+	program := parser.ParseProgram()
+	checkParseErrors(t, parser)
+
+	if len(program.Statements) != 1 {
+		t.Fatalf("program has not enough statements. got=%d", len(program.Statements))
+	}
+
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("program.Statements[0] is not ast.ExpressionStatement. got=%T", program.Statements[0])
+	}
+
+	literal, ok := stmt.Expression.(*ast.IntegerLiteral)
+	if !ok {
+		t.Fatalf("exp not *ast.IntegerLiteral. got=%T", stmt.Expression)
+	}
+
+	if literal.Value != 5 {
+		t.Fatalf("literal.Value not %s. got=%d", "foobar", literal.Value)
+	}
+
+	if literal.TokenLiteral() != "5" {
+		t.Fatalf("literal.TokenLiteral not %s. got=%s", "foobar", literal.TokenLiteral())
+	}
+}
