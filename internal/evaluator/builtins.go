36246aa9479f84fee8be4435ea05ee2121e649fb
gargalloeric <erickmontes555@gmail.com>
2025-10-09 11:39:45 +0200

    feat(builtin): add puts builtin function

--- internal/evaluator/builtins.go
+++ internal/evaluator/builtins.go
@@ -1,6 +1,10 @@
 package evaluator
 
-import "github.com/gargalloeric/monkey/internal/object"
+import (
+	"fmt"
+
+	"github.com/gargalloeric/monkey/internal/object"
+)
 
 var builtins = map[string]*object.Builtin{
 	"len": {
@@ -95,6 +99,15 @@ var builtins = map[string]*object.Builtin{
 			newElements[length] = args[1]
 
 			return &object.Array{Elements: newElements}
+		},
+	},
+	"puts": {
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+
+			return NULL
 		},
 	},
 }
